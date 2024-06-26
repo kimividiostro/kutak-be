68ae15fdbf7d3575cb84ef6174170672c1571c26
Milan <milance@pop-os.localdomain>
2024-06-26 22:31:53 +0200

    Established DB connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+mongoose.connect("mongodb://127.0.0.1:27017/kutak_dobre_hrane_db");
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("db connection ok");
+});
+
+
 app.listen(4000, () => console.log(`Express server running on port 4000`));
